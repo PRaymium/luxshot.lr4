a8d933d28e15de9925cfbf3a2340d9d5ad3b9e27
PRaymium <90473035+PRaymium@users.noreply.github.com>
2021-12-19 13:34:08 +0300

    update

--- templates/header.php
+++ templates/header.php
@@ -24,19 +24,19 @@
                 </nav>
             <?php else : ?>
 
-                <div class="nav-burger-menu hidden" id="nav-burger">
+                <div class="nav-burger-menu hidden-menu" id="nav-burger">
                     <div class="burger-menu__line burger-menu__line1"></div>
                     <div class="burger-menu__line burger-menu__line2"></div>
                     <div class="burger-menu__line burger-menu__line3"></div>
                 </div>
-                <nav id="nav-menu" class="nav-menu hidden">
-                    <div class="nav-menu__title hidden">Меню</div>
-                    <ul class="nav-menu__list hidden">
-                        <li class="nav-menu__list-li hidden">
-                            <div href="#" class="nav-menu__list-item hidden welcome">Здравствуйте, <?= $_SESSION['user']['name'] ?></div>
+                <nav id="nav-menu" class="nav-menu hidden-menu">
+                    <div class="nav-menu__title hidden-menu">Меню</div>
+                    <ul class="nav-menu__list hidden-menu">
+                        <li class="nav-menu__list-li hidden-menu">
+                            <div href="#" class="nav-menu__list-item hidden-menu welcome">Здравствуйте, <?= $_SESSION['user']['name'] ?></div>
                         </li>
-                        <li class="nav-menu__list-li hidden">
-                            <a href="logout.php" class="nav-menu__list-item hidden">Выйти</a>
+                        <li class="nav-menu__list-li hidden-menu">
+                            <a href="logout.php" class="nav-menu__list-item hidden-menu">Выйти</a>
                         </li>
                     </ul>
                 </nav>
